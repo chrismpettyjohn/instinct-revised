2c6c3c8555d70c1a14d44eb0b3f72e36c83704ea
Chris Pettyjohn <cpettyjohn@drivestl.com>
2021-02-22 20:10:59 -0600

    Clean up dependencies

--- packages/backend/plugin/user-guestbook/src/index.ts
+++ packages/backend/plugin/user-guestbook/src/index.ts
@@ -1,2 +1,4 @@
-export * from './email.module';
-export * from './email.service';
+export * from './user-guestbook.controller';
+export * from './user-guestbook.dto';
+export * from './user-guestbook.module';
+export * from './user-guestbook.pipe';
