1be321d5d0657260b165645683dd266607125853
Chris Pettyjohn <cpettyjohn@drivestl.com>
2021-02-28 12:34:49 -0600

    Add forum home page scaffold

---
+++ packages/frontend/hableet-theme/src/pages/forum/forum-home/list-sections/ListSections.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Card} from '../../../../components/card/Card';
+
+export function ListSections() {
+  return (
+    <Card header="List Sections">
+      <p>List Sections</p>
+    </Card>
+  );
+}
--- packages/frontend/hableet-theme/src/pages/index.ts
+++ packages/frontend/hableet-theme/src/pages/index.ts
@@ -2,4 +2,5 @@ import '../index.scss';
 import './auth';
 import './community';
 import './error';
+import './forum';
 import './home';
