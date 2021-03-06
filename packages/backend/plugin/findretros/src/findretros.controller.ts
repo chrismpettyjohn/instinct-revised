aa78520198fc9ad70a30cc9d7cdd4c956007bfb4
Chris Pettyjohn <cpettyjohn@drivestl.com>
2021-03-05 20:50:09 -0600

    prettier

--- packages/backend/plugin/findretros/src/findretros.controller.ts
+++ packages/backend/plugin/findretros/src/findretros.controller.ts
@@ -6,12 +6,10 @@ import {GetSession, HasSession} from '@instinct-api/session';
 @Controller('findretros')
 @HasSession()
 export class FindRetrosController {
-
   constructor(private readonly findRetrosService: FindRetrosService) {}
 
   @Get()
   async didUserVote(@GetSession() user: UserEntity): Promise<boolean> {
     return this.findRetrosService.didIPVote(user.ipCurrent);
   }
-
 }
