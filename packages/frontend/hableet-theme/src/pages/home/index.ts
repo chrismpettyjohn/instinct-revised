e5706157644cc9e40433663cfef0ed9ce121108b
Chris Pettyjohn <cpettyjohn@drivestl.com>
2021-03-05 22:45:05 -0600

    Add hotel rules page

--- packages/frontend/hableet-theme/src/pages/home/index.ts
+++ packages/frontend/hableet-theme/src/pages/home/index.ts
@@ -2,4 +2,5 @@ import './disconnected';
 import './home';
 import './play';
 import './preferences';
+import './game-rules/GameRules';
 import './user-profile';
--- packages/frontend/hableet-theme/src/pages/updates/ListUpdates.tsx
+++ packages/frontend/hableet-theme/src/pages/updates/ListUpdates.tsx
@@ -1,10 +1,11 @@
-import React, {useContext} from 'react';
 import './ListUpdates.scss';
+import React, {useContext} from 'react';
 import {Paper} from '@material-ui/core';
-import {configContext, setURL} from '@instinct-web/core';
 import {Card} from '../../components/card/Card';
+import {serverUpdates} from './ListUpdates.const';
 import {makeStyles} from '@material-ui/core/styles';
 import {UserLayout} from '../../components/layout/user';
+import {configContext, setURL} from '@instinct-web/core';
 import {MiniJumbotron} from '../../components/mini-jumbotron/MiniJumbotron';
 import {
   Timeline,
@@ -15,7 +16,6 @@ import {
   TimelineSeparator,
   TimelineDot,
 } from '@material-ui/lab';
-import {ServerUpdate} from './ListUpdates.types';
 
 setURL('updates', <ListUpdates />);
 
@@ -34,79 +34,6 @@ const useStyles = makeStyles(theme => ({
 export function ListUpdates() {
   const {config} = useContext(configContext);
   const classes = useStyles();
-
-  const serverUpdates: ServerUpdate[] = [
-    {
-      date: 'Mar 04',
-      icon: 'sparkles',
-      content: (
-        <>
-          <h5>Website Improvements</h5>
-          <ul>
-            <li>FindRetros Voting implemented</li>
-            <li>Revamped Staff Page</li>
-            <li>Revamped High Scores Page</li>
-          </ul>
-        </>
-      ),
-    },
-    {
-      date: 'Feb 29',
-      icon: 'bug',
-      content: (
-        <>
-          <h5>Bug fixes</h5>
-          <ul>
-            <li>Flash client crashing in some rooms</li>
-            <li>Flash application blue screen freeze</li>
-            <li>Page title showing "Habbo"</li>
-          </ul>
-        </>
-      ),
-    },
-    {
-      date: 'Feb 28',
-      icon: 'wrench',
-      content: (
-        <>
-          <h5>Server Updates</h5>
-          <p>
-            You can now stay up to date with the latest changes by going to the
-            new <b>Updates</b> page.
-          </p>
-          <h5>Who's Online</h5>
-          <p>The home page now shows all of the online users.</p>
-        </>
-      ),
-    },
-    {
-      date: 'Feb 27',
-      icon: 'browser',
-      content: (
-        <>
-          <h5>Nex Desktop</h5>
-          <p>
-            We launched our new <b>Nex Desktop</b> app! You can use it by
-            switching your client type in the top right and following the
-            instructions on the download page.
-          </p>
-        </>
-      ),
-    },
-    {
-      date: 'Feb 23',
-      icon: 'vial',
-      content: (
-        <>
-          <h5>Nex Revamp</h5>
-          <p>
-            We launched the new and improved Nex with a custom website and the
-            latest HTML5 browser available, Nitro!
-          </p>
-        </>
-      ),
-    },
-  ];
 
   return (
     <UserLayout>
