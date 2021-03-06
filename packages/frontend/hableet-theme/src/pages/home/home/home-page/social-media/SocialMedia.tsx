c57b4e86f3de8129fe83a89925981ad86d25ff33
Chris Pettyjohn <cpettyjohn@drivestl.com>
2021-03-05 22:57:00 -0600

    Improve social media link design

--- packages/frontend/hableet-theme/src/pages/home/home/home-page/social-media/SocialMedia.tsx
+++ packages/frontend/hableet-theme/src/pages/home/home/home-page/social-media/SocialMedia.tsx
@@ -1,3 +1,4 @@
+import './SocialMedia.scss';
 import React, {useContext} from 'react';
 import {configContext, Icon} from '@instinct-web/core';
 import {Card} from '../../../../../components/card/Card';
@@ -6,11 +7,23 @@ export function SocialMedia() {
   const {config} = useContext(configContext);
   return (
     <Card className="mt-4" header="Social Media">
-      <a href={config.discordURL} target="_blank">
-        <Icon className="fa-3x" family="fab" type="discord" />
+      <a className="social-media-row" href={config.discordURL} target="_blank">
+        <div className="d-flex">
+          <Icon className="fa-3x" family="fab" type="discord" />
+          <div className="ml-4">
+            <h4>Discord</h4>
+            <p style={{marginTop: -5}}>Use dis to stay up 2 date</p>
+          </div>
+        </div>
       </a>
-      <a className="ml-4" href={config.twitterURL} target="_blank">
-        <Icon className="fa-3x" family="fab" type="twitter" />
+      <a className="social-media-row" href={config.twitterURL} target="_blank">
+        <div className="d-flex">
+          <Icon className="fa-3x" family="fab" type="twitter" />
+          <div className="ml-4">
+            <h4>Twitter</h4>
+            <p style={{marginTop: -5}}>We tweet shit okrt</p>
+          </div>
+        </div>
       </a>
     </Card>
   );
