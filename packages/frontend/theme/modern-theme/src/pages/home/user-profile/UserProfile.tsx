c495aaad0b520400d392840a99552871e95f0776
Chris Pettyjohn <cpettyjohn@drivestl.com>
2021-03-14 21:27:30 -0500

    delete profiles

--- packages/frontend/theme/modern-theme/src/pages/home/user-profile/UserProfile.tsx
+++ packages/frontend/theme/modern-theme/src/pages/home/user-profile/UserProfile.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import './UserProfile.scss';
-import {Rooms} from './rooms';
 import {useRoute} from 'wouter';
-import {Friends} from './friends';
-import {UserContainer} from './user-container';
 import {UserLayout} from '../../../components/layout/user';
-import {FavoriteVideo} from './favorite-video/FavoriteVideo';
-import {Loading, setURL, useFetchUserByUsername} from '@instinct-web/core';
-import {MiniJumbotron} from '../../../components/old/mini-jumbotron/MiniJumbotron';
+import {setURL, useFetchUserByUsername} from '@instinct-web/core';
+import {Card, CardContent, Grid, Typography} from '@material-ui/core';
 
 setURL('profile/:username', <UserProfile />);
 
@@ -17,37 +13,14 @@ export function UserProfile() {
 
   return (
     <UserLayout>
-      <Loading isLoading={profile === undefined}>
-        <div className="page-content">
-          <div className="row">
-            <div className="col-12">
-              <MiniJumbotron>
-                <div className="row">
-                  <div className="col">
-                    <h1>{profile?.user?.username}'s Profile</h1>
-                    <p>{profile?.user?.motto}</p>
-                  </div>
-                  <div className="col text-right">
-                    {profile?.user?.rank?.permissions?.websiteShowStaff && (
-                      <img src="/img/staff.gif" height={100} />
-                    )}
-                  </div>
-                </div>
-              </MiniJumbotron>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-lg-4">
-              <UserContainer profile={profile} />
-              <FavoriteVideo profile={profile} />
-            </div>
-            <div className="col">
-              <Friends profile={profile} />
-              <Rooms profile={profile} />
-            </div>
-          </div>
-        </div>
-      </Loading>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Typography variant="h4">
+            {profile?.user?.username}'s Profile
+          </Typography>
+          <Typography variant="subtitle1">"{profile?.user?.motto}"</Typography>
+        </Grid>
+      </Grid>
     </UserLayout>
   );
 }
--- packages/frontend/theme/modern-theme/src/pages/home/user-profile/user-container/UserContainer.tsx
+++ packages/frontend/theme/modern-theme/src/pages/home/user-profile/user-container/UserContainer.tsx
@@ -1,87 +1,30 @@
 import Moment from 'moment';
 import React from 'react';
 import {UserProfileWidgetProps} from '../';
-import {Avatar, Icon} from '@instinct-web/core';
+import {Avatar as UserAvatar, Icon} from '@instinct-web/core';
 import {DetailsContainer} from './details-container';
+import {Avatar, Card, CardContent, Grid, Typography} from '@material-ui/core';
 
 export function UserContainer({profile}: UserProfileWidgetProps) {
   return (
-    <aside id="profile" className="default-section p-0">
-      <div
-        className="profile-header"
-        style={{background: "url('https://i.imgur.com/dmY7Sbg.png')"}}
-      >
-        <div
-          className="header-content flex-container flex-vertical-center p-4"
-          style={{backgroundColor: '#47AEE'}}
-        >
-          <div style={{overflow: 'hidden'}}>
-            <Avatar
-              look={profile?.user.figure || ''}
-              direction={2}
-              headDirection={2}
-              style={{marginLeft: -40}}
-              size="l"
-            />
-          </div>
-          <div className="header-details">
-            <div className="header-description" />
-            <div className="profile-icon">
-              <img
-                alt="credits"
-                src="/img/icons/credits.png"
-                style={{position: 'initial'}}
+    <Card style={{background: '#272B34', color: 'white'}}>
+      <CardContent>
+        <Grid container spacing={3}>
+          <Grid item xs={4}>
+            <Avatar style={{height: 100, width: 100, marginTop: '10%'}}>
+              <UserAvatar
+                look={profile?.user?.figure}
+                size="l"
+                style={{height: 160}}
               />
-              &nbsp;
-              {profile?.user.credits.toLocaleString()}
-            </div>
-            <div className="profile-icon">
-              <img
-                alt="duckets"
-                src="/img/icons/duckets.png"
-                style={{position: 'initial'}}
-              />
-              &nbsp;
-              {profile?.user.pixels.toLocaleString()}
-            </div>
-            <div className="profile-icon">
-              <img
-                alt="diamonds"
-                src="/img/icons/diamonds.png"
-                style={{position: 'initial'}}
-              />
-              &nbsp;{profile?.user.points.toLocaleString()}
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="profile-content p-4">
-        <div className="details-container">
-          <Icon type="hotel" />
-          Currently&nbsp;
-          {profile?.user.online ? (
-            <strong className="text-success">online</strong>
-          ) : (
-            <strong className="text-danger">offline</strong>
-          )}
-        </div>
-        <DetailsContainer icon="user" header="Registered Since">
-          {Moment(profile?.user.joinDate).format('MM/DD/YYYY')}
-        </DetailsContainer>
-        <DetailsContainer icon="door-open" header="Last Logged In">
-          {Moment(profile?.user.lastLoginDate).format('MM/DD/YYYY')}
-        </DetailsContainer>
-        <DetailsContainer icon="headphones-alt">
-          <a
-            className="youtube-link"
-            href="https://youtu.be/GfxcnX7XWfg"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            https://youtu.be/GfxcnX7XWfg
-          </a>
-        </DetailsContainer>
-      </div>
-    </aside>
+            </Avatar>
+          </Grid>
+          <Grid item xs={8}>
+            <Typography variant="">{profile?.user?.username}</Typography>
+            <Typography variant="caption">"{profile?.user?.motto}"</Typography>
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
   );
 }
