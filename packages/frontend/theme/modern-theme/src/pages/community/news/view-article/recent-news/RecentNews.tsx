519c29829da48ba535317aa3898f5a4624faa584
Chris Pettyjohn <cpettyjohn@drivestl.com>
2021-03-14 20:19:46 -0500

    Update view article page

--- packages/frontend/theme/modern-theme/src/pages/community/news/view-article/recent-news/RecentNews.tsx
+++ packages/frontend/theme/modern-theme/src/pages/community/news/view-article/recent-news/RecentNews.tsx
@@ -1,10 +1,8 @@
-import './RecentNews.scss';
-import Moment from 'moment';
 import {Link} from 'wouter';
 import {Article} from '@instinct-prj/interface';
 import React, {useEffect, useState} from 'react';
-import {Card} from '../../../../../components/old/card/Card';
-import {articleService, Loading} from '@instinct-web/core';
+import {articleService} from '@instinct-web/core';
+import {Card, CardContent, Typography, Paper} from '@material-ui/core';
 import {defaultRecentNewsState, RecentNewsState} from './RecentNews.types';
 
 export function RecentNews() {
@@ -24,32 +22,40 @@ export function RecentNews() {
   }, []);
 
   return (
-    <Card header="Hotel Updates">
-      <Loading isLoading={state.isLoading}>
-        <div
-          className="related-articles-container"
-          style={{maxHeight: 200, overflowY: 'scroll'}}
-        >
+    <Card style={{background: '#272B34', color: 'white'}}>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="h2">
+          Recent News
+        </Typography>
+        <div style={{maxHeight: 200, overflowY: 'scroll'}}>
           {state.articles.map(article => (
-            <Link
-              className="related-article-container"
-              key={article.id}
-              to={`/community/news/${article.id}`}
-            >
-              <div
-                className="related-article-thumbnail"
-                style={{backgroundImage: `url(${article.thumbnailImage})`}}
-              />
-              <div className="related-article-details">
-                <div className="related-article-title">{article.title}</div>
-                <div className="related-article-date">
-                  {Moment.unix(article.datePosted).format('MMM DD, YYYY')}
+            <Link key={article.id} to={`/community/news/${article.id}`}>
+              <Paper
+                elevation={3}
+                style={{
+                  background: '#1e2128',
+                  cursor: 'pointer',
+                  color: 'white',
+                  marginBottom: '2.5%',
+                }}
+              >
+                <div style={{display: 'flex'}}>
+                  <img
+                    src={article.thumbnailImage}
+                    style={{width: 75, height: 75}}
+                  />
+                  <div style={{padding: 4}}>
+                    <Typography variant="subtitle1">{article.title}</Typography>
+                    <Typography variant="body2y">
+                      {article.description}
+                    </Typography>
+                  </div>
                 </div>
-              </div>
+              </Paper>
             </Link>
           ))}
         </div>
-      </Loading>
+      </CardContent>
     </Card>
   );
 }
