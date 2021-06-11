cd702e8cb66b0e3bc1ecf425231ca5f2446ae98c
Chris Pettyjohn <cpettyjohn@drivestl.com>
2021-06-11 13:48:52 -0500

    Scaffold for Bobba theme

--- demo/web/src/index.tsx
+++ demo/web/src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { InstinctWeb } from './InstinctWeb';
 import '@instinct-web/admin/dist/admin.css';
 import '@instinct-web/core/dist/frontend.css';
-// import '@instinct-theme/ares/dist/themes.css';
 import '@instinct-theme/classic/dist/public/css/classic-theme.css';
 
 
