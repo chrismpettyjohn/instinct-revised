09bcc914cfb2bd7bbcc2a3440dd137f8c7e22a5a
Chris Pettyjohn <cpettyjohn@drivestl.com>
2021-06-11 15:10:25 -0500

    Remove redundant import

--- demo/web/src/index.tsx
+++ demo/web/src/index.tsx
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom';
 import { InstinctWeb } from './InstinctWeb';
 import '@instinct-web/admin/dist/admin.css';
 import '@instinct-web/core/dist/frontend.css';
-import '@instinct-theme/classic/dist/public/css/classic-theme.css';
-
 
 ReactDOM.render(<InstinctWeb />, document.getElementById('root'));
