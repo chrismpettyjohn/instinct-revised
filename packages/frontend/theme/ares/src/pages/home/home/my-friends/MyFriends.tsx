6ca3525cfe4640312db19446f74fff0950e34786
Chris Pettyjohn <cpettyjohn@drivestl.com>
2021-06-17 17:14:58 -0500

    Ares - Friends widget on me page

--- packages/frontend/theme/ares/src/pages/home/home/my-friends/MyFriends.tsx
+++ packages/frontend/theme/ares/src/pages/home/home/my-friends/MyFriends.tsx
@@ -1,47 +1,76 @@
-import React from 'react';
+import React, {useContext, useState} from 'react';
 import './MyFriends.scss';
-import {Avatar} from '@instinct-web/core';
+import {
+  Avatar,
+  sessionContext,
+  useFetchUserByUsername,
+} from '@instinct-web/core';
 
 export function MyFriends() {
+  const {user} = useContext(sessionContext);
+  const [filter, setFilter] = useState('');
+  const profile = useFetchUserByUsername(user!.username);
+
+  if (!user) {
+    return null;
+  }
+
+  const filteredFriends = profile?.friends.filter(_ =>
+    _.username.toLowerCase().includes(filter)
+  );
+
   return (
     <div className="card friends">
       <div className="card-body d-flex flex-column">
         <div className="d-inline-flex align-items-center">
           <span className="title flex-fill">Friends</span>
-
           <div id="search" className="input-group bg-white w-50">
             <input
               className="form-control"
               placeholder="Search..."
               type="search"
               maxLength={12}
+              value={filter}
+              onChange={e => setFilter(e.target.value)}
             />
             <div className="input-group-text bg-white" id="search-icon">
               <i className="fa fa-search" />
             </div>
           </div>
         </div>
-
         <div id="friends" className="row">
-          <div className="d-inline-flex flex-column h-100 w-100 justify-content-center">
-            <h3 className="align-self-center">
-              <i className="fa unlink" />
-            </h3>
-            <h4 className="align-self-center">Friend</h4>
-          </div>
-
           <div className="col-xl-4 col-lg-4 col-md-12 col-sm-12">
-            <div className="card friend mb-2 d-flex flex-row align-items-center">
-              <Avatar look="lg-275-80.hd-185-2.ch-3050-78-77.hr-831-45" />
+            {filteredFriends?.map(_ => (
+              <div
+                className="card friend mb-2 d-flex flex-row align-items-center"
+                key={`friend_${_.id}`}
+              >
+                <Avatar look={_.figure} />
 
-              <div className="user-info d-inline-flex flex-column text-ellipsis">
-                <span className="username text-ellipsis mb-0">Bob</span>
-                <span className="motto text-ellipsis">
-                  Riding the wave of life
-                </span>
+                <div className="user-info d-inline-flex flex-column text-ellipsis">
+                  <span className="username text-ellipsis mb-0">
+                    {_.username}
+                  </span>
+                  <span className="motto text-ellipsis">{_.motto}</span>
+                </div>
               </div>
-            </div>
+            ))}
           </div>
+          {filteredFriends?.length === 0 && (
+            <div className="d-inline-flex flex-column h-100 w-100 justify-content-center">
+              <h4 className="align-self-center">No Friends</h4>
+            </div>
+          )}
+          {profile?.friends?.length === 0 && (
+            <div className="d-inline-flex flex-column h-100 w-100 justify-content-center">
+              <h4 className="align-self-center">No Friends</h4>
+            </div>
+          )}
+          {!profile && (
+            <div className="d-inline-flex flex-column h-100 w-100 justify-content-center">
+              <h4 className="align-self-center">Loading...</h4>
+            </div>
+          )}
         </div>
       </div>
     </div>
