d214d6c6d05d1ee61a4cee6dfc1ce94e3bc82ef9
Chris Pettyjohn <cpettyjohn@drivestl.com>
2021-06-20 01:05:07 -0500

    fix broken links

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/WELCOME">
+            to="/docs/intro">
             Learn More
           </Link>
         </div>
