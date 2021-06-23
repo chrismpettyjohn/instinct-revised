54b69e395777356e3b7a26c42a7418c798615db8
Chris Pettyjohn <cpettyjohn@drivestl.com>
2021-06-23 16:01:13 -0500

    Downgrade Typescript due to React oneOf issue

--- demo/web/src/index.tsx
+++ demo/web/src/index.tsx
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import { InstinctWeb } from './InstinctWeb';
 import '@instinct-web/admin/dist/admin.css';
 import '@instinct-web/core/dist/frontend.css';
+import '@instinct-theme/cosmic/dist/public/css/cosmic-theme.css';
 
 ReactDOM.render(<InstinctWeb />, document.getElementById('root'));
